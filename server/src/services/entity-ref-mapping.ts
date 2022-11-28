9ca8e9b21a0c4ab75dc384e25db1b9c7d1fecf45
Hariharan-Neutrinos <hari.haran@neutrinos.co>
2022-11-28 11:58:51 +0530

    del

--- server/src/services/entity-ref-mapping.ts
+++ server/src/services/entity-ref-mapping.ts
@@ -1,7 +1,3 @@
 import * as _EN_ez1516135p from '../entities/erd/entity_1';
-import * as _EN_0cnqd3leq0 from '../entities/erd/entity_2';
 
-export const EntityMap = {
-  _EN_ez1516135p: _EN_ez1516135p['entity_1'],
-  _EN_0cnqd3leq0: _EN_0cnqd3leq0['entity_2'],
-};
+export const EntityMap = { _EN_ez1516135p: _EN_ez1516135p['entity_1'] };
