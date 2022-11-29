7832908f5538f8e6a41de7f26964b8a7a6a0cf3d
Hariharan-Neutrinos <hari.haran@neutrinos.co>
2022-11-29 11:13:11 +0530

    ids changes done

--- server/src/entities/erd/entity_1.ts
+++ server/src/entities/erd/entity_1.ts
@@ -6,12 +6,4 @@ import { PrimaryGeneratedColumn, Entity, Column } from 'typeorm'; //_splitter_
 export class entity_1 {
   @PrimaryGeneratedColumn({ name: 'id' })
   id: number;
-  @Column({ name: 'name1', nullable: false, type: 'text', primary: false })
-  name1: string;
-  @Column({ name: 'name2', nullable: false, type: 'text', primary: false })
-  name2: string;
-  @Column({ name: 'name3', nullable: false, type: 'text', primary: false })
-  name3: string;
-  @Column({ name: 'name4', nullable: false, type: 'text', primary: false })
-  name4: string;
 }
