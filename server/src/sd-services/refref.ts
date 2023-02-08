bacd3603c39eccd23789d84e18f6fb9e3488a3b0
Hariharan-Neutrinos <hari.haran@neutrinos.co>
2023-02-08 20:54:53 +0530

    del

---
+++ server/src/sd-services/refref.ts
@@ -0,0 +1,153 @@
+// _neu_generated_code__dont_modify_directly_
+let instance = null;
+//CORE_REFERENCE_IMPORTS
+//append_imports_start
+
+import { StatusCodes as httpStatusCodes } from 'http-status-codes'; //_splitter_
+import * as cookieParser from 'cookie-parser'; //_splitter_
+import { Readable } from 'stream'; //_splitter_
+import { setInterval } from 'timers'; //_splitter_
+import { Issuer, custom } from 'openid-client'; //_splitter_
+import * as crypto from 'crypto'; //_splitter_
+import * as url from 'url'; //_splitter_
+import { SDBaseService } from '../services/SDBaseService'; //_splitter_
+import { Middleware } from '../middleware/Middleware'; //_splitter_
+import * as settings from '../config/config'; //_splitter_
+import log from '../utils/Logger'; //_splitter_
+import { TracerService } from '../services/TracerService'; //_splitter_
+//append_imports_end
+export class refref {
+  private sdService = new SDBaseService();
+  private app;
+  private serviceBasePath: string;
+  private generatedMiddlewares: Object;
+  private serviceName: string;
+
+  private globalTimers: any;
+  private constructor(
+    app,
+    generatedeMiddlewares,
+    routeCall,
+    middlewareCall,
+    globalTimers
+  ) {
+    this.serviceName = 'refref';
+    this.app = app;
+    this.serviceBasePath = this.app.settings.base;
+    this.generatedMiddlewares = generatedeMiddlewares;
+    this.globalTimers = globalTimers;
+  }
+
+  static getInstance(
+    app?,
+    generatedeMiddlewares?,
+    routeCall?,
+    middlewareCall?,
+    globalTimers?
+  ) {
+    if (!instance) {
+      instance = new refref(
+        app,
+        generatedeMiddlewares,
+        routeCall,
+        middlewareCall,
+        globalTimers
+      );
+    }
+    instance.mountCalls(routeCall, middlewareCall);
+    return instance;
+  }
+
+  private mountCalls(routeCall, middlewareCall) {
+    if (routeCall) {
+      this.mountAllPaths();
+      this.mountAllListeners();
+    }
+    if (middlewareCall) {
+      this.generatedMiddlewares[this.serviceName] = {};
+      this.mountAllMiddlewares();
+      this.mountTimers();
+    }
+  }
+
+  async mountAllListeners() {
+    try {
+      //append_listeners
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  async mountTimers() {
+    try {
+      //appendnew_flow_refref_TimerStart
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  private mountAllMiddlewares() {
+    log.debug('mounting all middlewares for service :: refref');
+
+    //appendnew_flow_refref_MiddlewareStart
+  }
+  private mountAllPaths() {
+    log.debug('mounting all paths for service :: refref');
+    //appendnew_flow_refref_HttpIn
+  }
+  //   service flows_refref
+
+  async sd_HKM0VPicoPbrvMZ2(parentSpanInst, ...others) {
+    const spanInst = this.tracerService.createSpan(
+      'sd_HKM0VPicoPbrvMZ2',
+      parentSpanInst
+    );
+    try {
+      var bh: any = {
+        local: {},
+      };
+      bh = this.sdService.__constructDefault(bh, bh.web.req, bh.web.res);
+      this.tracerService.sendData(spanInst, bh);
+      //appendnew_next_sd_HKM0VPicoPbrvMZ2
+      return (
+        // formatting output variables
+        {
+          input: {},
+          local: {},
+        }
+      );
+    } catch (e) {
+      return await this.errorHandler(
+        bh,
+        e,
+        'sd_HKM0VPicoPbrvMZ2',
+        spanInst,
+        'sd_HKM0VPicoPbrvMZ2'
+      );
+    }
+  }
+
+  //appendnew_flow_refref_start
+
+  //appendnew_node
+
+  async errorHandler(bh, e, src) {
+    console.error(e);
+    bh.error = e;
+    bh.errorSource = src;
+
+    if (
+      false
+      /*appendnew_next_Catch*/
+    ) {
+      return bh;
+    } else {
+      if (bh.web.next) {
+        bh.web.next(e);
+      } else {
+        throw e;
+      }
+    }
+  }
+  //appendnew_flow_refref_Catch
+}
